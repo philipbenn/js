5ee524905c8e1497963b8c2d30c43e2c52996a1b
philipbenn <phil658c@stud.kea.dk>
2023-09-21 12:50:33 +0200

    adjustsments added setup for day4 ex1,2

---
+++ JSDay4/ex2/pages/allCars/allCars.js
@@ -0,0 +1,18 @@
+
+import { SERVER_URL } from "../../settings.js"
+
+export async function initAllCars() {
+  console.log("initAllCars")
+
+  const cars = await getCars()
+  
+  const listItems = cars.map(car => `
+        <li>${car.model} , ${car.brand} </li>
+    `).join("")
+    document.getElementById("cars").innerHTML = listItems
+}
+
+async function getCars(){
+  const cars = await fetch(SERVER_URL).then(res=>res.json())
+  return cars
+}
---
+++ JSDay4/ex2/pages/editCar/editCar.js
@@ -0,0 +1,5 @@
+
+export function initEditCar(){
+    console.log("initEditCar")
+  }
+  
